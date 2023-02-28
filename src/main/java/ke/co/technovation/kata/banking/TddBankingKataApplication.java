bb3081ec096ee8fc9845bc0163a2674b5d846cae
lloyd kioko <ch.abene254@gmail.com>
2023-02-28 16:43:32 +0300

    #1 Setup Project - Added CI Workflow - Sample failing test

---
+++ src/main/java/ke/co/technovation/kata/banking/TddBankingKataApplication.java
@@ -0,0 +1,4 @@
+package ke.co.technovation.kata.banking;
+
+public class TddBankingKataApplication {
+}
---
+++ src/test/java/ke/co/technovation/kata/banking/TddBankingKataApplicationTests.java
@@ -0,0 +1,15 @@
+package ke.co.technovation.kata.banking;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;;
+
+
+public class TddBankingKataApplicationTests {
+
+
+    @Test
+    void contextLoads() {
+        assertThat(true).isTrue();
+    }
+}
